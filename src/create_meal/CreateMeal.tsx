f76e98c7338732aec2417acb23ef6c135731aa39
Giovani Schiar <giovanischiar@gmail.com>
2022-02-28 20:11:16 -0800

    Sketching a create meal screen

--- src/create_meal/CreateMeal.tsx
+++ src/create_meal/CreateMeal.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, View, Text, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
 
 export const CreateMeal = () => {
+	const [description, setDescription] = useState('');
+	const { screen, form, descriptionInput, imageInput } = styles;
+
+	const onSelectImage = () => {
+		Alert.alert('TODO');
+	}
+
+	const onSubmit = () => {
+		Alert.alert('TODO', `{description: \"${description}\"}`)
+	}
+
 	return (
-		<SafeAreaView>
-			<View>
-				
+		<SafeAreaView style={screen}>
+			<View style={form}>
+				<TouchableOpacity onPress={onSelectImage} style={imageInput}>
+					<Text style={{textAlign: 'center'}}>Select Image</Text>
+				</TouchableOpacity>
+				<TextInput 
+					style={descriptionInput}
+					placeholder="Date and Time (will be filled automatically by images's date))"
+				/>
+				<TextInput 
+					style={descriptionInput}
+					placeholder="Description" 
+					multiline
+					onChangeText={setDescription}
+				/>
+				<Button onPress={onSubmit} title="Done" />
 			</View>
-			<Text>Ois</Text>
 		</SafeAreaView>
 	);
 }
@@ -23,3 +46,27 @@ CreateMeal.options = {
     }
   }
 }
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1, 
+		alignItems: 'center'
+	},
+
+	form: {
+		margin: 15, 
+		backgroundColor: '#ededed', 
+		padding: 15
+	},
+
+	imageInput: {
+		width: 374, 
+		height: 248, 
+		borderWidth: 1, 
+		justifyContent: 'center'
+	},
+
+	descriptionInput: {
+		marginTop: 15
+	}
+});
