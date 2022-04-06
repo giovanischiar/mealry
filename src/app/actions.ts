8b86d7f12bf19b964b5a804083bb9bc23334072a
Giovani Schiar <giovanischiar@gmail.com>
2022-04-05 20:54:17 -0700

    Now you can edit the day date and the list updates the meal in a new or existent day

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -15,10 +15,14 @@ export const loadMeals = () => {
 }
 
 export const addMeal = (id: string, date: number, images: string[], description: string) => {
-	console.log(id, date, images, description);
+	const daysSinceEpoch = (millisecondsSinceEpoch: number): number => {
+		return Math.floor(millisecondsSinceEpoch/(24 * 60 * 60 * 1000));
+	}
 	const dayMeals = store.getState().dayMeals;
 	let newDaymeals = [...dayMeals]
 	let newDayMeal = {day: date, meals: [{id: id != "" ? id : String(Math.random() * 1000), date, images, description}]};
+	let dateWasChanged = false;
+	let dayMealEmpty = -1;
 	if (id != "") {
 		for (var i = 0; i < dayMeals.length; i++) {
 			const dayMeal = dayMeals[i];
@@ -26,23 +30,35 @@ export const addMeal = (id: string, date: number, images: string[], description:
 				const meal = dayMeal.meals[j];
 				if (meal.id === id) {
 					const newMeal = newDayMeal.meals[0]
-					newDaymeals[i].meals = [...newDaymeals[i].meals.slice(0, j), newMeal, ...newDaymeals[i].meals.slice(j+1, newDaymeals[i].meals.length)]
+					if (newDayMeal.day !== dayMeal.day) {
+						dateWasChanged = true;
+						newDaymeals[i].meals = [...newDaymeals[i].meals.slice(0, j), ...newDaymeals[i].meals.slice(j+1, newDaymeals[i].meals.length)];
+						if (newDaymeals[i].meals.length === 0) {
+							dayMealEmpty = i
+						}
+						break;
+					}
+					newDaymeals[i].meals = [...newDaymeals[i].meals.slice(0, j), newMeal, ...newDaymeals[i].meals.slice(j+1, newDaymeals[i].meals.length)];
 				}
 			}
 		}
-	} else {
-		let dayAlreadyRegistered = false
+	}
+
+	if (dateWasChanged || id === "") {
+		let dayAlreadyRegistered = false;
 		for (var i = 0; i < dayMeals.length; i++) {
-			if (newDaymeals[i].day.toString().substr(0, 7) === newDayMeal.day.toString().substr(0, 7)) {
+			if (daysSinceEpoch(dayMeals[i].day) === daysSinceEpoch(newDayMeal.day)) {
 				newDaymeals[i].meals = [...dayMeals[i].meals, ...newDayMeal.meals];
-				dayAlreadyRegistered = true
+				dayAlreadyRegistered = true;
 			}
 		}
 		if (!dayAlreadyRegistered) {
 			newDaymeals = [...newDaymeals, newDayMeal];
 		}
 	}
-
+	if (dayMealEmpty !== -1) {
+		newDaymeals = [...newDaymeals.slice(0, dayMealEmpty), ...newDaymeals.slice(dayMealEmpty+1, newDaymeals.length)];
+	}
 	return {type: ACTIONS.LOAD_MEALS, payload: newDaymeals};
 }
 
